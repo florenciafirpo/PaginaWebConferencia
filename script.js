6d0f63e78245ca67b5bdd9865f83b052e2f84913
Florencia Firpo <florenciafirpoalday@hotmail.com>
2023-06-04 16:04:58 -0300

    agregando funciones con JS

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function calcularPrecio(){
+function calcularPrecio() {
     var total;
-    let cantidad = cantidadEntradas()
-    let categoria = personas()
+    let cantidad = cantidadEntradas();
+    let categoria = personas();
 
+    if (categoria === "1") {
+      total = cantidad * (200 * 0.2);
+    } else if (categoria === "2") {
+      total = cantidad * (200 * 0.5);
+    } else if (categoria === "3") {
+      total = cantidad * (200 * 0.85);
+    }
 
+    const apuntarPrecio = document.getElementById("totalAPagar");
+    apuntarPrecio.innerHTML = "Total a pagar: $" + total;
+}
+
+function personas() {
+    const persona = document.getElementById("categoria");
+    const categoria = persona.value;
+    return categoria;
+}
+
+function cantidadEntradas() {
+    const apuntadorPersonas = document.getElementById("cantidad");
+    const valorEntradas = apuntadorPersonas.value;
+    const numeroAsistentes = toNumber(valorEntradas);
+    return numeroAsistentes;
+}
+
+function toNumber(valor) {
+    const apuntadorValor = Number(valor);
+    return apuntadorValor;
 }
